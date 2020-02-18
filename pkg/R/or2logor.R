519f80ae6b051fc79f76710336ee76c663aefebf
manuelao <manuelao@b1c1774c-e9a7-4141-b269-6dd821e7a813>
2020-02-18 17:06:34 +0000

    minor change in doc. of the flexible func.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sl4bayesmeta@11 b1c1774c-e9a7-4141-b269-6dd821e7a813

--- pkg/R/or2logor.R
+++ pkg/R/or2logor.R
@@ -1,6 +1,4 @@
 
-# TODO: rename the function?
-  
 or2logor <- function(tab){
   # Function transforming the results on the OR (exp(mu)) scale to results on the logOR (mu) scale
   # Remark: The invariance of the Hellinger distance to any monotone transformation guarantees that S and L values do not change.
@@ -11,12 +9,10 @@ or2logor <- function(tab){
   tab_logOR_scale[, "95CrI_post_OR_low"] <- log(tab_logOR_scale[, "95CrI_post_OR_low"])
   tab_logOR_scale[, "95CrI_post_OR_up"] <- log(tab_logOR_scale[, "95CrI_post_OR_up"])
   tab_logOR_scale[, "length_95CrI_post_OR"] <- tab_logOR_scale[, "95CrI_post_OR_up"]-tab_logOR_scale[, "95CrI_post_OR_low"]
+  
   # rename the transformed entries
   colnames(tab_logOR_scale)[5:8] <- c("median_post_mu", "95CrI_post_mu_low", "95CrI_post_mu_up",
                                       "length_95CrI_post_mu") 
-  # colnames(obj_logOR_scale) <- c("U", "tail_prob", "par_val", "MRLMC", "median_post_mu", "95CrI_post_mu_low", "95CrI_post_mu_up",
-  #                                "length_95CrI_post_mu", "median_post_tau", "95CrI_post_tau_low", "95CrI_post_tau_up", "length_95CrI_post_tau",
-  #                                "L_mu", "L_tau", "S_mu", "S_tau")
   return(tab_logOR_scale)
 }
 
